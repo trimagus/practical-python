129e82f1ca07a1822f8ab8002277ba4cf1fb333a
trimagus <trimagus8@gmail.com>
2021-02-25 20:45:02 +0100

    első commit

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
-# bounce.py
-#
-# Exercise 1.5
+#bounce.py
+
+i=1
+height = 100
+
+while i<=10:
+	height = height*3/5
+	print (i,':', round(height,4))
+	i=i+1
+	
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment = 1000
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+total_paid = 0.0
+month = 0
+
+
+
+while principal > payment:
+    if (month >= extra_payment_start_month) & (month <= extra_payment_end_month):   
+        principal = principal * (1+rate/12) - payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
+    else:    
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+    print (month+1, round(total_paid,2), round(principal,2))
+    month = month + 1
+    
+print('Total paid', round(total_paid,2), 'Month', month, 'rest', round(principal,2))
+
